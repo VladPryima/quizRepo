96eee77dedc70633a3b35a6e6aa4d1ecc31cd046
VladPryima <114105594+VladPryima@users.noreply.github.com>
2023-01-11 15:35:18 +0200

    add comments

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,35 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
-const Quiz = ({ showQuiz, question, quizs, checkAnswer, correctAnswer, selectedAnswer, questionIndex, nextQuestion, showTheResult, start, timeLeft, setTimeLeft, setStart }) => {
+const Quiz = ({ quizs, showQuiz, question,  checkAnswer, correctAnswer, selectedAnswer, questionIndex, nextQuestion, showTheResult, startTimer, timeLeft, setTimeLeft, setStartTimer, setTime, time }) => {
 
-    
-   //const [timeLeft, setTimeLeft] = useState(sec);
-   const seconds = timeLeft;
-  
- 
-   let arr = [];
+   let seconds = timeLeft;
+   
+
    
    useEffect(() => {
-    
+    //Если startTimer изменился - начинаем отсчет
      const interval = setInterval( () => {
-        start && setTimeLeft((timeLeft) => timeLeft >= 1 ? timeLeft - 1 : 0) 
+        startTimer && setTimeLeft((timeLeft) => timeLeft >= 1 ? timeLeft - 1 : 0); //Запускаем interval каждую секунду и уменьшаем timeLeft
+        
+        
     }, 
     1000);
-    
-   arr.push(interval);
-   console.log(arr);
     
    return () => {
     clearInterval(interval);
    }
 
-   }, [start])
+   }, [startTimer]);
+
+   useEffect(( )=> {
+    setTime(time + 1); 
+   }, [timeLeft]);
+
 
    
   
 
     return (
-        
         <section className="bg-dark text-white" style={{ display: `${showQuiz ? 'block' : 'none'}` }}>
             <a className="choosePage" href='https://vladpryima.github.io/home/'>&#129080; До вибору проєктів</a>
             <div className="container">
@@ -37,19 +37,22 @@ const Quiz = ({ showQuiz, question, quizs, checkAnswer, correctAnswer, selectedA
                     <div className="col-lg-8">
                         <div className="card p-4" style={{ background: 'rgb(23 68 78)', borderColor: 'yellow' }}>
                             <div className="d-flex justify-content-between gap-md-3">
-                                <h5 className='mb-2 fs-normal lh-base'>{question?.question}</h5>
+                                <h5 className='mb-2 fs-normal lh-base'>{question.question}</h5>  {/*Питання*/}
                                 <h5>Таймер: {seconds}</h5> 
-                                <h5 style={{ color: '#60d600', width: '100px', textAlign: 'right' }}>{quizs.indexOf(question) + 1} / {quizs?.length}</h5>
+                                <h5 style={{ color: '#60d600', width: '100px', textAlign: 'right' }}>{quizs.indexOf(question) + 1} / {quizs?.length}</h5>  {/*Номер питання*/}
                             </div>
                             <div>
-                                {
+                            {/*Создаем список вопросов из пропса question*/}
+                                { 
+                                //Тут можно перетасовать вопросы
+                                
                                     question?.options?.map((item, index) => <button
                                         key={index}
                                         className={`option w-100 text-start btn text-white py-2 px-3 mt-3 rounded btn-dark ${correctAnswer === item && 'bg-success'}`}
                                         onClick={(event) => {
                                             
-                                           setStart(false);
-                                            checkAnswer(event, item);
+                                            setStartTimer(false); //Зупиняємо таймер при виборі відповіді
+                                            checkAnswer(event, item); //Перевіряємо відповідь
                                         }
                                         
                                         }
@@ -61,7 +64,8 @@ const Quiz = ({ showQuiz, question, quizs, checkAnswer, correctAnswer, selectedA
 
                             { 
 
-                               
+                                //Условный рендеринг в зависимости от длинны quizs.length 
+                                //Если questionIndex != quizs.length, значит продолжаем, иначе - показываем результати && 
                                 (questionIndex + 1) !== quizs.length && seconds > 1 ? 
                                     <button className='btn py-2 w-100 mt-3 bg-primary text-light fw-bold' onClick={nextQuestion} disabled={!selectedAnswer}>Наступне питання</button>
                                     :
